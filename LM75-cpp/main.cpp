c052d66eb1fd4db4d138b8b151dfa9336150e6ba
Matthieu BORDAS <matthieu.bordas@ynov.com>
2019-03-28 11:44:21 +0100

    mise en place du nouveaux fichier

--- LM75-cpp/main.cpp
+++ LM75-cpp/main.cpp
@@ -1,10 +1,9 @@
-#include "mbed.h"
+	#include "mbed.h"
 	#include "zest-radio-atzbrf233.h"
 	#include "MQTTNetwork.h"
 	#include "MQTTmbed.h"
 	#include "MQTTClient.h"
 	#include "rtos.h"
-
 
 	const char* dashboard_host = "io.adafruit.com";
 	const int dashboard_port = 1883;
@@ -18,16 +17,26 @@
 
 	static Ticker ticker;
 
-	//dÃ©finit les fonctions
+	//définit les fonctions
 	static void yieldAuto(MQTT::Client<MQTTNetwork, Countdown> *c);						//ping le serveur
-	static void every_Seconds(MQTT::Client<MQTTNetwork, Countdown> *c);					//rÃ©alise les taches
+	static void every_Seconds(MQTT::Client<MQTTNetwork, Countdown> *c);					//réalise les taches
 	static float Recup_Temperature_Capteur();		//recupere la temp
 	static float Recup_Humidite_Capteur();			//recupere l'hum
 	static void sendDatas(float t, float h, MQTT::Client<MQTTNetwork, Countdown> *c);
-	static void test();
+	static void ledState(MQTT::Client<MQTTNetwork, Countdown> *c);
 
-	void test(){
+	I2C i2c(I2C1_SDA, I2C1_SCL);
+	uint8_t lm75_adress = 0x48 << 1;
+
+	AnalogIn humAnalog(ADC_IN2);
+
+
+
+	void ledState(MQTT::Client<MQTTNetwork, Countdown> *c){
 		 printf("alive \n");
+
+		// topicEtatLed
+
 	}
 
 	void yieldAuto(MQTT::Client<MQTTNetwork, Countdown> *c){
@@ -36,11 +45,11 @@
 
 	void every_Seconds(MQTT::Client<MQTTNetwork, Countdown> *c){
 
-		//rÃ©cupere temp et hum
+		//récupere temp et hum
 		float temperature = Recup_Temperature_Capteur();
 		float humidite = Recup_Humidite_Capteur();
 
-		//envois les donnÃ©es sur le serveur
+		//envois les données sur le serveur
 		sendDatas(temperature, humidite, c);
 
 		//affichage sur console
@@ -52,26 +61,63 @@
 
 	float Recup_Temperature_Capteur(){
 
-		// rÃ©cupere la tension du capteur
+		// récupere la tension du capteur
+		// float v = tempAnalog.read();
 
-		// conversion
+		char cmd[2];
+		cmd[0] = 0x00; // adresse registre temperature
+		i2c.write(lm75_adress, cmd, 1);
+		i2c.read(lm75_adress, cmd, 2);
 
-		//retourne la valeur
-		return 25.0;
+		float temperature = ((cmd[0] << 8 | cmd[1] ) >> 7) * 0.5;
+
+		return temperature;
+
 	}
 
 	float Recup_Humidite_Capteur(){
 
-		// rÃ©cupere la tension du capteur
+		// récupere la tension du capteur
+		float v = humAnalog.read();
 
 		// conversion
+		v *= 3.3;
 
-		// retourne la valeur
-		return 51.5;
+		float min = 0;
+		float max = 3.3;
+
+		//retourne la valeur
+		return min * 100 / max;
 	}
 
 	void sendDatas(float temperature, float humidite, MQTT::Client<MQTTNetwork, Countdown> *c){
-		//rc = client.publish(topicGraphTemp, message);
+
+		MQTT::Message message;
+
+		// Envois le taux de temp
+		char bufT[15];
+		sprintf(bufT, "%f", temperature);
+
+		message.qos = MQTT::QOS0;
+		message.retained = false;
+		message.dup = false;
+		message.payload = (void*)bufT;
+		message.payloadlen = strlen(bufT)+1;
+
+		c->publish(topicGraphTemp, message);
+
+		// Envois le taux d'humidité
+		char bufH[15];
+		sprintf(bufT, "%f", humidite);
+
+		message.qos = MQTT::QOS0;
+		message.retained = false;
+		message.dup = false;
+		message.payload = (void*)bufH;
+		message.payloadlen = strlen(bufH)+1;
+
+		c->publish(topicGraphHum, message);
+
 	}
 
 
@@ -169,7 +215,7 @@
 		//les events queues
 		event_queue.call_every(250, callback(yieldAuto, &client));
 		event_queue.call_every(2000, callback(every_Seconds, &client));
-		event_queue.call_every(500, test);
+		event_queue.call_every(500, callback(ledState, &client));
 
 		// start events queues (fonction bloquante)
 		event_queue.dispatch_forever();
